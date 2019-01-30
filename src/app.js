b67e952547dfde7f396584cddb06d0279f839fae
Artem <dunaev.aa@yandex.ru>
2019-01-30 23:04:22 +0500

    add cards and modals

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const modalOpenButtons = document.querySelectorAll(".card__button");
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close");
+const escKeyCode = 27;
+
+modalOpenButtons.forEach(element =>
+  element.addEventListener("click", () => modalOpen())
+);
+
+const modalOpen = () => {
+  modal.classList.add("modal--opened");
+  modalCloseButton.addEventListener("click", () => modalClose());
+};
+
+const modalClose = () => {
+  modal.classList.remove("modal--opened");
+};
+
+document.addEventListener("keydown", evt => {
+  if (
+    modal.classList.contains("modal--opened") &&
+    event.keyCode === escKeyCode
+  ) {
+    modal.classList.remove("modal--opened");
+  }
+});
